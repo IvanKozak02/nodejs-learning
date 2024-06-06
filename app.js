d30cb47928fb832bf7f8425167f172b32c2b2ae0
ivan <ivasykkozak611@gmail.com>
2024-06-06 22:56:22 +0300

    Figure out with os module

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello everyone!!!');
+const greeting = require('./greeting');     // import module greeting.js; //todo greeting - {exports from greeting module}
+
+greeting.sayHi(greeting.name)
+
+console.log(greeting)
+
+// console.log(process)
+// console.log(__dirname)
+//
+// console.log(module)
+
+
+
+// Module
+
+//todo module = this file
+
+// module.exports = sayHi  -  one thing export
+
---
+++ greeting.js
@@ -0,0 +1,12 @@
+const name = 'Petro';
+
+function sayHi(name) {
+    console.log(`Hi, ${name}`)
+}
+
+sayHi('Ivan')
+
+module.exports = {
+    name,
+    sayHi
+}
